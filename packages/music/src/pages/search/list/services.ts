078f09d64bc18b6f2fcd5249ce4bc04eca50f532
yunliang <804408086@qq.com>
2024-12-11 20:31:07 +0800

    clean

--- packages/music/src/pages/search/list/services.ts
+++ packages/music/src/pages/search/list/services.ts
@@ -4,7 +4,7 @@ import { request } from 'lyr';
 export const queryByKeyword = async (keywords: string): Promise<any> => {
   const {
     result: { songs },
-  }: any = await request.get(`/music.163/search?keywords=${keywords}`, {});
+  }: any = await request.get(`/music.163/search?keywords=${keywords}&limit=60`, {});
   return request.get(
     `/music.163/song/detail?ids=${songs.map((i) => i.id).join(',')}`,
     {},
